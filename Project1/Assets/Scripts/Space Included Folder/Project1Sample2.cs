1ee1231cd2cbc5de9e6353104997915db78e2c65
mattak <mattak.me@gmail.com>
2024-10-08 12:15:57 +0900

    :seedling: formatcheck

--- Project1/Assets/Scripts/Space Included Folder/Project1Sample2.cs
+++ Project1/Assets/Scripts/Space Included Folder/Project1Sample2.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Project1Sample2 : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-
     }
 
     // Update is called once per frame
